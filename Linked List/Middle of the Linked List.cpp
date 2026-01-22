6dca233db0f2dfe77e15ce88269e2894f30a5a7c
Santunu Kaysar Mahin <136067964+SantunuMahin@users.noreply.github.com>
2026-01-22 09:56:14 +0600

    Update and rename Middle of the Linked List to Middle of the Linked List.cpp

--- Linked List/Middle of the Linked List.cpp
+++ Linked List/Middle of the Linked List.cpp
@@ -1,10 +1,11 @@
+/**
 link: https://leetcode.com/problems/middle-of-the-linked-list/
 
 Name: 876. Middle of the Linked List
 
 Given the head of a singly linked list, return the middle node of the linked list.
 If there are two middle nodes, return the second middle node.
-
+*/
 Solution : 
 
 class Solution {
