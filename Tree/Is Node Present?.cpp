f5597eb90100bc807727d0a1e2a21a8355936457
Santunu Kaysar Mahin <136067964+SantunuMahin@users.noreply.github.com>
2026-01-26 13:07:51 +0600

    Create Is Node Present?.cpp

---
+++ Tree/Is Node Present?.cpp
@@ -0,0 +1,39 @@
+/************************************************************
+
+    Following is the Binary Tree node structure
+
+    template <typename T>
+    class BinaryTreeNode
+    {
+        public :
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data)
+        {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+bool isNodePresent(BinaryTreeNode<int> *root, int x) {
+    // Write your code here
+   queue<BinaryTreeNode<int>*>q;
+   q.push(root);
+   while(!q.empty()){
+       // ber kora
+       BinaryTreeNode<int>* f = q.front();
+       q.pop();
+
+       //kaj 
+       if(f->data == x)return true;
+
+       if(f->left)q.push(f->left);
+       if(f->right)q.push(f->right);
+   }
+   return false;
+    
+}
